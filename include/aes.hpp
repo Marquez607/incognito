3c99964c67ea311a92c7b38effec1f404651a656
marquez607 <marquezjones607@gmail.com>
2021-02-19 19:46:22 -0500

    started aes.cpp

--- include/aes.hpp
+++ include/aes.hpp
@@ -1,7 +1,7 @@
 /*******************************************************************
- * Name: aes256 
+ * Name: aes
  * Author: Marquez Jones
- * Desc: implementation of AES256 encryption for incognito project
+ * Desc: implementation of AES256 encryption/decryption for incognito project
  *******************************************************************/
 #pragma once
 
@@ -11,3 +11,39 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <openssl/err.h>
+
+class __aes__
+{
+pubic: 
+  static const int AES_KEY_SIZE = 32; //bytes
+  static const int AES_BLOCK_SIZE = 16; //bytes
+protected: 
+  __aes__() {}
+  unsigned char *key = NULL;
+  unsigned char *iv  = NULL; //init vector
+}
+
+
+class DecryptionAES : public __aes__
+{
+public:
+  DecryptionAES();
+  DecryptionAES();
+  //_out msg malloc, return -1 on failure
+  int Decrypt(int _flen, unsigned char *_encrypted, unsigned char *_out);
+  int DecryptSetKey(unsigned char *key);
+}
+
+class EncryptionAES : public __aes__
+{
+public:
+  EncryptionAES();
+  ~EncryptionAES();
+  //_out malloc, return -1 on failure
+  int Encrypt(int _msg_len,unsigned char *_msg, unsigned char *_out);
+  int EncryptMsgSize();
+  void EncrypteSetKey(unsigned char *key);
+private:
+  int _msg_size = -1;
+  int GenerateIV(void); //generated randomly each time
+}
--- src/aes.cpp
+++ src/aes.cpp
@@ -7,4 +7,69 @@
 #include <string.h>
 #include <stdexcept>
 #include <unistd.h>
-#include "aes256.hpp"
+#include "aes.hpp"
+
+/***************************** DECRYPTION ****************************/
+DecryptionAES::DecryptionAES()
+{
+  this->iv = new unsigned char[AES_KEY_SIZE];
+  this->key = new unsigned char[AES_KEY_SIZE];
+}
+
+
+DecryptionAES::~DecryptionAES()
+{
+  free(this->iv);
+  free(this->key);
+}
+
+int DecryptionAES::Decrypt(int _flen, unsigned char *_encrypted, unsigned char *_out)
+{
+}
+
+int DecryptionAES::DecryptSetKey(unsigned char *key)
+{
+}
+
+/***************************** ENCRYPTION ***************************/
+EncryptionAES::EncryptionAES()
+{
+  this->iv = new unsigned char[AES_KEY_SIZE];
+  this->key = new unsigned char[AES_KEY_SIZE];
+}
+
+EncryptionAES::~EncryptionAES()
+{
+  free(this->iv);
+  free(this->key);
+}
+
+int EncryptionAES::Encrypt(int _msg_len,unsigned char *_msg, unsigned char *_out)
+{
+  
+  this->_msg_size = _msg_len + ((AES_BLOCK_SIZE-_msg_len)%AES_BLOCK_SIZE);
+  _out = new unsigned char[this->_msg_size];
+  EVP_CIPHER_CTX *enc_ctx = EVP_CIPHER_CTX_new();
+  EVP_EncryptInit(enc_ctx, EVP_aes_256_cbc(),_msg,this->key,this->iv);
+  // TODO: finish
+}
+
+int EncryptionAES::EncryptMsgSize()
+{
+}
+
+void EncryptionAES::EncrypteSetKey(unsigned char *key)
+{
+
+  
+}
+
+int EncryptionAES::GenerateIV()
+{
+    if(!RAND_Bytes(this->iv,AES_KEY_SIZE)){
+        throw std::runtime_error("AES IV Generation Failed");
+    }
+}
+
+
+
